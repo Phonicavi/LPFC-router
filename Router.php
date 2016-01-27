5136c22cf015c8a5c8b12fc4e015db95dbc2b661
Phonicavi <mardpazin7san@hotmail.com>
2016-01-28 01:18:10 +0800

    test for publish
    调试composer

--- Router.php
+++ Router.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LPFCRouter\Hermes;
+namespace Hermes\Router;
 
 
 class Router {
---
+++ show.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Hermes\Router as Router;
+
+p = new Router();
+
+echo "nothing...";
